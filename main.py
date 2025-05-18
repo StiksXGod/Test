8ae4c7bbe0cc1b501d60a6a5756fb0cdf3a08290
Павел Мудров <stiks@MacBook-Pro-Pavel.local>
2025-05-19 00:28:48 +0300

    done

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 from fastapi import FastAPI
+from prometheus_client import start_http_server, Summary, generate_latest
+from starlette.responses import Response
 
 app = FastAPI()
+
+@app.get("/metrics")
+def metrics():
+    return Response(generate_latest(), media_type="text/plain")
 
 @app.get("/")
 def read_root():
