c01a6a83fee273a04d7776d7fe9fbeef84547959
Павел Мудров <stiks@MacBook-Pro-Pavel.local>
2025-05-19 03:12:37 +0300

    done

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
 from fastapi import FastAPI
-from prometheus_client import start_http_server, Summary, generate_latest
-from starlette.responses import Response
 from prometheus_fastapi_instrumentator import Instrumentator
 
 app = FastAPI()
